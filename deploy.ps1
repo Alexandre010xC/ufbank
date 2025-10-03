f591262af8c2cf13e3a4978b13c17983ff814866
Alexandre Coelho <alexandre.010c@gmail.com>
2025-10-03 00:53:09 -0300

    hello-world nestjs + docker + kubernetes

---
+++ deploy.ps1
@@ -0,0 +1,13 @@
+docker build -t nestjs-app:latest .
+
+minikube start --memory=512mb --cpus=1
+
+minikube docker-env | Invoke-Expression
+
+docker build -t nestjs-app:latest .
+
+kubectl apply -f k8s-deployment.yaml
+
+kubectl rollout status deployment/nestjs-app
+
+minikube service nestjs-service --url
